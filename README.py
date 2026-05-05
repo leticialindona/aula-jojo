74742138d76ec6129a0e58aa5a8fe864dd94d4f5
leticialindona <leticiafinocchi@gmail.com>
2026-05-05 15:17:00 -0300

    Update README.py

--- README.py
+++ README.py
@@ -9,3 +9,9 @@ partido = st.text_input('coloque aqui a sigla do partido')
 if partido:
   filtrado = df[df['partido'].str.upper() == partido.upper()]
   st.dataframe(filtrado)
+
+estado = st.text_input('coloque aqui a sigla do estado')
+
+if estado:
+  filtrado = df[df['partido'].str.upper() == partido.upper()]
+  st.dataframe(filtrado)
