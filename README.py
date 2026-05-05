eaa66a633542e4194602c113bbeb72b8ebcbe005
leticialindona <leticiafinocchi@gmail.com>
2026-05-05 14:53:33 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Análise de Deputados 2018 🗳️")
+st.title("Análise de Deputados 2018")
 
 # Upload do arquivo
 arquivo = st.file_uploader("Envie seu CSV", type=["csv"])
@@ -22,7 +22,7 @@ if arquivo is not None:
     })
 
     # Verifica se deu certo
-    if "votos" in df.columns and "sexo" in df.columns:
+    if "votos" in df.columns:
         
         # Top 10
         top = df.sort_values(by="votos", ascending=False).head(10)
@@ -30,14 +30,5 @@ if arquivo is not None:
         st.subheader("Top 10 mais votados")
         st.dataframe(top[["nome", "votos", "sexo"]])
 
-        # Contagem
-        contagem = top["sexo"].value_counts()
-
-        st.subheader("Distribuição por gênero")
-        st.write(contagem)
-
-        maioria = contagem.idxmax()
-        st.success(f"Maioria: {maioria}")
-
     else:
-        st.error("Não encontrei as colunas de votos ou sexo ")
+        st.error("Não encontrei as colunas de votos")
