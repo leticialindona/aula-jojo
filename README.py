093168a89a5f1eaeb4c05a904fba81bece19b9cd
leticialindona <leticiafinocchi@gmail.com>
2026-05-05 15:04:55 -0300

    Update README.py

--- README.py
+++ README.py
@@ -3,4 +3,9 @@ import pandas as pd
 
 df = pd.read_csv("deputados_2022.csv")
 
-st.title('analise dos deputados por partido')
+st.title('Analise dos deputados por partido')
+partido = st.text_input('coloque aqui a sigla do partido')
+
+if partido:
+  filtrado = df[df['partido'].str.upper() == partido.upper()]
+  st.dataframe(filtrado)
